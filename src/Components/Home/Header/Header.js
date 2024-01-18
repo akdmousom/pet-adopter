4fb12da4c9a794f8efd3cb84d4af69e4c4271b4a
rasel khan <tunnerbd2016@gmail.com>
2024-01-19 02:28:04 +0600

    [design](mainLayout) APP Layout Design Done

---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,21 @@
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+
+const Header = () => {
+    return (
+        <div className=" text-center md:flex items-center justify-between px-5 md:px-16 py-2 bg-[#0A303A] text-white">
+            <div className="">
+                <p>Call us: 747-800-9880  Opening Hours: 7:00 am - 9:00 pm (Mon - Sun)</p>
+            </div>
+            <div className="md:flex  space-y-2 justify-center items-center gap-2">
+                <p>Follow :</p>
+                <div className="flex gap-5 justify-center">
+                    <FaFacebook />
+                    <FaTwitter />
+                    <FaInstagram />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/Components/Shared/NavBar/NavBar.js
+++ src/Components/Shared/NavBar/NavBar.js
@@ -1,7 +1,90 @@
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { CgProfile } from "react-icons/cg";
+import logo from "../../../../public/logo.png"
+import { usePathname } from "next/navigation";
+import Header from "@/components/Home/Header/Header";
+
+
+
+
+const NavLinks = [
+    { id: 1, name: 'Home', path: '/' },
+    { id: 2, name: 'Blog', path: '/blog' },
+    { id: 3, name: 'About', path: '/about' },
+    { id: 4, name: 'Contact', path: '/contact' },
+];
+
 const NavBar = () => {
+
+    const pathname = usePathname();
+
+    const isActive = (path) => pathname === path;
     return (
         <div>
-            
+            <Header />
+            <div className="drawer bg-[#ffe3e1] text-black shadow-md ">
+                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex justify-evenly flex-col">
+                    {/* Navbar */}
+                    <div className="w-full navbar ">
+                        <div className="flex-none lg:hidden">
+                            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            </label>
+                        </div>
+                        <div className="flex justify-evenly w-full  ">
+                            <Image src={logo} height={30} width={120} alt="logo" />
+
+
+                            <div className="flex-none   text-center hidden md:block">
+                                <ul className="flex items-center gap-5">
+                                    {/* Navbar menu content here */}
+                                    {NavLinks.map((link) => {
+                                        return (
+                                            <li key={link.id}>
+                                                <Link
+                                                    href={link.path}
+                                                    className={isActive(link.path) ? ' bg-[#F04336] text-lg px-3 py-2 rounded-lg  font-bold text-white mx-2' : 'text-lg font-bold'}
+                                                >
+                                                    {link.name}
+                                                </Link>
+                                            </li>
+                                        );
+                                    })}
+
+                                </ul>
+                            </div>
+                            <div className="flex gap-5  items-center ">
+                                <button className="text-lg text-white rounded-full font-medium px-5 py-2 bg-[#F04336]">Adopt Here</button>
+                                <CgProfile className="text-3xl text-[#F04336]" />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Page content here */}
+
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 min-h-full bg-base-200">
+                        {/* Sidebar content here */}
+                        {NavLinks.map((link) => {
+                            return (
+                                <li key={link.id}>
+                                    <Link
+                                        href={link.path}
+                                        className={isActive(link.path) ? ' bg-[#F04336] text-lg px-3 py-2 rounded-lg  font-bold text-white mx-2' : 'text-lg font-bold'}
+                                    >
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
