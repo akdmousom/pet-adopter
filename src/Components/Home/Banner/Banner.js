6c818a4da584964252d3a358fab4fa97fbedd194
AKD <contact.mousom@gmail.com>
2024-01-18 20:33:05 +0600

    [Structure](Folder): Structured done

---
+++ src/Components/Home/Banner/Banner.js
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Shared/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NavBar;
