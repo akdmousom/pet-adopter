709dacd7eff832b7195e032e9bab359dac25d9de
rasel khan <tunnerbd2016@gmail.com>
2024-01-19 18:23:19 +0600

    [Design](Home page and  Contact page design)

--- src/Components/SectionTitle/SectionTitle.js
+++ src/Components/SectionTitle/SectionTitle.js
@@ -1,9 +1,9 @@
 
 
-const SectionTitle = ({ }) => {
+const SectionTitle = ({ heading }) => {
     return (
         <div>
-
+            <h1 className="text-3xl py-5 mx-auto text-center  w-96 font-bold text-[#200d0c]">{heading}</h1>
         </div>
     );
 };
