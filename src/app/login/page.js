f8b0ee2f620be4bfeec3ca9a4fd7d471d12251e6
AKD <contact.mousom@gmail.com>
2024-01-23 17:41:23 +0600

    [Design](Change) Login and register page design change

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,34 @@
-import React from 'react';
-
 const page = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="hero max-w-[1366px] min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Login to your dashboard. Here you can get all functionality for adopting any pets</p>
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
