1e461d80ad5418583c22f22b4235ca892b44ba8a
lokman hakim <lokmanhakimshawon3@gmail.com>
2024-01-25 17:33:44 +0600

    [add](login and register page) add google and github authentication with next-auth

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,13 @@
+'use client'
+import { signIn } from "next-auth/react";
 import Link from "next/link";
-
+import googleIcon from '../../../public/images/google.svg'
+import githubIcon from '../../../public/images/github.svg'
+import Image from 'next/image'
 const page = () => {
 
     const userData = async (formData) => {
-        'use server'
+        // 'use server'
         const rawFormData = {
             userEmail: formData.get('email'),
             userPassword: formData.get('password'),
@@ -44,6 +48,15 @@ const page = () => {
                         </div>
                         <div className="form-control mt-6">
                             <button type="submit" className="btn bg-[#f04336] text-white  hover:bg-black">Login</button>
+                        </div>
+                        <hr/>
+                        <p className="text-center">OR</p>
+                        <hr/>
+                        <div className="form-control ">
+                            <button onClick={()=>signIn('google')} type="submit" className="btn hover:bg-black hover:text-white gap-5"><Image src={googleIcon} alt='google icon' width={30} /> Login With Google</button>
+                        </div>
+                        <div className="form-control ">
+                            <button onClick={()=>signIn('github')} type="submit" className="btn hover:bg-black hover:text-white gap-5"><Image src={githubIcon} alt='google icon' width={30} /> Login With Github</button>
                         </div>
                     </form>
                 </div>
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,10 +1,14 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
-
+import googleIcon from '../../../public/images/google.svg'
+import githubIcon from '../../../public/images/github.svg'
+import Image from 'next/image'
+import {signIn} from 'next-auth/react'
 const page = () => {
 
     const userData = async (formData) => {
-        'use server'
+        // 'use server'
         const rawFormData = {
             fullName: formData.get('userName'),
             userEmail: formData.get('userEmail'),
@@ -64,6 +68,15 @@ const page = () => {
 
                         <div className="form-control mt-6">
                             <button type='submit' className="btn bg-[#f04336] text-white hover:bg-black">Register</button>
+                        </div>
+                        <hr/>
+                        <p className="text-center">OR</p>
+                        <hr/>
+                        <div className="form-control ">
+                            <button onClick={()=>signIn('google')} type="submit" className="btn hover:bg-black hover:text-white gap-5"><Image src={googleIcon} alt='google icon' width={30} /> Login With Google</button>
+                        </div>
+                        <div className="form-control ">
+                            <button onClick={()=>signIn('github')} type="submit" className="btn hover:bg-black hover:text-white gap-5"><Image src={githubIcon} alt='google icon' width={30} /> Login With Github</button>
                         </div>
                     </form>
                 </div>
