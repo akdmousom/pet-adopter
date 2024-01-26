7c06945d642871a9f60674acbb8be18bacb40c83
lokman hakim <lokmanhakimshawon3@gmail.com>
2024-01-26 20:45:12 +0600

    [add](adopt page) add adopt page

---
+++ src/app/adoptHerePage/page.js
@@ -0,0 +1,12 @@
+import AdoptHerePage from '@/Components/AdoptHerePage/AdoptHerePage';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+          <AdoptHerePage/>
+        </div>
+    );
+};
+
+export default page;
