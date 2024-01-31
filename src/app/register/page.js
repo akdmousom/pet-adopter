d6f8d208a62ccce1efa5f70977622b7b45659fe5
programmingquestioner <contact.mousom@gmail.com>
2024-01-31 14:58:07 +0600

    [Create User](Post Request) Now user can creat his/her account dynamically

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -14,7 +14,8 @@ const page = () => {
             userAvater: formData.get('userAvater'),
             userPassword: formData.get('userPassword'),
         }
-
+        // Send post request for new user
+        
         const res = await fetch('http://localhost:5000/api/v1/userregistration', {
             method: 'POST',
             cache: 'no-cache',
