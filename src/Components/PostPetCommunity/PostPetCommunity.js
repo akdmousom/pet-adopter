38887f802094e645afe1eaa2baee9827391acda6
lokman hakim <lokmanhakimshawon3@gmail.com>
2024-02-14 01:27:19 +0600

    [update](post you community) update form and alert massege

--- src/Components/PostPetCommunity/PostPetCommunity.js
+++ src/Components/PostPetCommunity/PostPetCommunity.js
@@ -6,8 +6,8 @@ const PostPetCommunity = () => {
     const handleCommunityData=(e)=>{
         e.preventDefault();
         const event=e.target
-        const img=event.file.files[0]
-        const message=event.message.value
+        let img=event.file.files[0]
+        let message=event.message.value
         const imgbb_key='ceccd7f28cf74921bd1fb98402d6032f'
        
         const formData= new FormData()
@@ -17,31 +17,57 @@ const PostPetCommunity = () => {
             const image=res.data.data.display_url
             const postData={image,message}
             console.log(image,message)
-            axios.post('http://localhost:5000/api/v1/petCommunity',postData)
-            .then(res=>{
-                console.log(res)
+            if(message==='' || img===''){
                 Swal.fire({
-                    position: "top-end",
-                    icon: "success",
-                    title: "Your work has been saved",
+                    position: "center",
+                    icon: "warning",
+                    title: "Please fill up all inbox field",
                     showConfirmButton: false,
                     timer: 1500
                   });
+                  
+            }
+            else{
+                axios.post('http://localhost:5000/api/v1/petCommunity',postData)
+            .then(res=>{
+                console.log(res)
+                if(res.statusText==='OK'){
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Community Post Successfull",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                      event.file.value=''
+                      event.message.value=''
+
+                }
+                else{
+                    Swal.fire({
+                        position: "center",
+                        icon: "warning",
+                        title: "something was wrong !",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
             })
             .catch(error=>console.log(error,'error'))
+            }
         })
         .catch(error=>console.log(error,'error'))
     }
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
-   <div className="card shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+            <div className="hero min-h-screen bg-base-200 py-10">
+   <div className="card shrink-0  w-full max-w-lg mx-10 shadow-2xl bg-base-100">
       <form onSubmit={handleCommunityData} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Image</span>
           </label>
-          <input type="file" name='file' className="input input-bordered input-secondary" required />
+          <input type="file" name='file' className="input input-bordered input-secondary w-full" required />
         </div>
         <div className="form-control">
           <label className="label">
