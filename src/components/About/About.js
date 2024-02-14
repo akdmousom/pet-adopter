613a93ca78004f35cbe71a220ee5ab6df6497c52
MasudRana14 <iammasud34@gmail.com>
2024-02-14 21:53:55 +0600

    [Redesign](Our Partner section) Add Some Partner Logo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -55,11 +55,15 @@ const About = () => {
                 <div className="mb-20 lg:mb-40 px-4">
                     <h3 className="text-3xl lg:text-5xl text-[#F04336] text-center font-bold my-8">Our Partner</h3>
                     <Marquee>
-                        <div className="grid grid-cols-4 gap-5 justify-center items-center">
+                        <div className="flex gap-5 justify-center items-center">
                             <Image src="https://i.ibb.co/PrT58Qp/F1-Formula-1.png" width={200} height={600} alt="F1-Formula-1" className="hover:scale-110 hover:border-black border-2 transfrom duration-700 w-[150px] h-[150px] mx-auto object-cover" />
                             <Image src="https://i.ibb.co/3yn4PhJ/Louis-Vuitton-Print.png" width={200} height={600} alt="Louis Vuitton" className="hover:scale-110 hover:border-black border-2 transfrom duration-700 w-[150px] h-[150px] mx-auto object-cover" />
                             <Image src="https://i.ibb.co/D7R6575/STANLEY.png" width={200} height={600} alt="STANLEY" className="hover:scale-110 hover:border-black border-2 transfrom duration-700 w-[150px] h-[150px] mx-auto object-cover" />
                             <Image src="https://i.ibb.co/ChmWZBv/Play-Station.png" width={200} height={600} alt="Play Station" className="hover:scale-110 hover:border-black border-2 transfrom duration-700 w-[150px] h-[150px] mx-auto object-cover" />
+                            <Image src="https://i.ibb.co/N3hv1Jc/roboflow.jpg" width={200} height={600} alt="Play Station" className="hover:scale-110 hover:border-black border-2 transfrom duration-700 w-[150px] h-[150px] mx-auto object-cover" />
+                            <Image src="https://i.ibb.co/2dqTCr5/paws.png" width={200} height={600} alt="Play Station" className="hover:scale-110 hover:border-black border-2 transfrom duration-700 w-[150px] h-[150px] mx-auto object-cover" />
+                            <Image src="https://i.ibb.co/dLhBZSP/petclub.jpg" width={200} height={600} alt="Play Station" className="hover:scale-110 hover:border-black border-2 transfrom duration-700 w-[150px] h-[150px] mx-auto object-cover" />
+                            <Image src="https://i.ibb.co/BsB4bgK/netflix.png" width={200} height={600} alt="Play Station" className="hover:scale-110 hover:border-black border-2 mr-4 transfrom duration-700 w-[150px] h-[150px] mx-auto object-cover" />
                         </div>
                     </Marquee>
                 </div>
