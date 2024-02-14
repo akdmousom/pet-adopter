b8e6193e72551dfa8f5f33767c09bef5e530b060
lokman hakim <lokmanhakimshawon3@gmail.com>
2024-02-14 11:01:24 +0600

    [add](pet lover community) add data from api to pet lover community page.

--- src/Components/PetCommunity/PetCommunity.js
+++ src/Components/PetCommunity/PetCommunity.js
@@ -1,16 +1,24 @@
 'use client'
 import Image from "next/image";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const PetCommunity = () => {
     const [trueData,setStrueData]=useState(true)
+    const [postData,setPostData]=useState([])
 
      const story='Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.Hello Everyone This is my pet.'
+     useEffect(()=>{
+        axios.get('http://localhost:5000/api/v1/petCommunity')
+     .then(res=>setPostData(res.data))
+     .catch(error=>console.log(error,'error'))
      
+     },[])
+     console.log(postData)
     return (
-        <div className="max-w-[1366px] mx-auto my-10 p-5">
-
+        <div>
+            {postData.map(data=><div key={data._id} className="max-w-[1366px] mx-auto my-10 p-5">
         <div className="grid lg:grid-cols-1 md:grid-cols-1 gap-3">
 
             {/* Blog Card 1 */}
@@ -20,12 +28,12 @@ const PetCommunity = () => {
                     <div className="flex">
                         <div className="avatar mx-3">
                             <div className="w-12 rounded-full">
-                                <Image width={200} height={200} src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="" />
+                                <Image width={200} height={200} src={data.user_image} alt="" />
                             </div>
                         </div>
                         {/* Name & Time  */}
                         <div>
-                            <h3 className="text-base">Devil King</h3>
+                            <h3 className="text-base">{data.user_name}</h3>
                             <p>3hr ago</p>
                         </div>
                     </div>
@@ -33,16 +41,16 @@ const PetCommunity = () => {
                     <div className="lg:mx-4 my-3">
                       {
                         story.length <= 150?
-                        <h1>{story}</h1>
+                        <h1>{data.input_message}</h1>
                         :
                         trueData===true?
-                        <h1>{story.slice(0,150)}....<span className="text-gray-400 cursor-pointer" onClick={()=>setStrueData(false)}>Read more</span></h1>
+                        <h1>{data.input_message.slice(0,150)}....<span className="text-gray-400 cursor-pointer" onClick={()=>setStrueData(false)}>Read more</span></h1>
                         :
-                        <h1>{story}</h1>
+                        <h1>{data.input_message}</h1>
                       }
                     </div>
                 </div>
-                <figure><Image width={400} height={400} src="https://i.ibb.co/8nZpQhF/pet6.jpg" alt="Shoes" className="w-full" /></figure>
+                <figure><Image width={400} height={400} src={data.input_image} alt="Shoes" className="w-full" /></figure>
                 <div className="p-5 space-y-5">
 
                     {/* Like & Comment  */}
@@ -64,7 +72,8 @@ const PetCommunity = () => {
         </div>
 
 
-    </div>
+    </div>)}
+        </div>
     );
 };
 
