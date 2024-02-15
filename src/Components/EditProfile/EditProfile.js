f610fb1737331036d18506e54b684b1c7602b763
MasudRana14 <iammasud34@gmail.com>
2024-02-15 23:10:15 +0600

    [Add](function) Add handle click function

--- src/Components/EditProfile/EditProfile.js
+++ src/Components/EditProfile/EditProfile.js
@@ -2,6 +2,24 @@ import Image from "next/image";
 
 
 const EditProfile = () => {
+
+    const handleEdit = e=>{
+        e.preventDefault()
+        const form = e.target;
+        const name1 = form.name1.value;
+        const name2 = form.name2.value;
+        const email = form.email.value;
+        const address = form.address.value;
+        const contact = form.contact.value;
+        const city = form.city.value;
+        const state = form.state.value;
+        const password = form.password.value;
+        e.target.form.value=" "
+
+        console.log(name1,name2,email,address,contact,city,state,password);
+    }
+
+
     return (
         <div className="bg-red-200 my-10">
         <div className="p-4">
@@ -17,53 +35,53 @@ const EditProfile = () => {
 
             {/* Input Form  */}
             <div className="w-1/2 mx-auto my-10">
-                <form className="space-y-4">
+                <form onSubmit={handleEdit} className="space-y-4">
 
                     <div className="flex md:flex-row flex-col gap-5">
                         {/* First Name  */}
                         <label className="form-control w-full max-w-xs">
                             <span className="label-text-alt text-lg">First Name</span>
-                            <input type="text" placeholder="First Name" id="" className="p-1 border-2 " />
+                            <input type="text" name="name1" placeholder="First Name" id="" className="p-1 border-2 " />
                         </label>
                         {/* Last Name  */}
                         <label className="form-control w-full max-w-xs">
                             <span className="label-text-alt text-lg">Last Name</span>
-                            <input type="text" placeholder="Last Name" id="" className="p-1 border-2 " />
+                            <input type="text" name="name2" placeholder="Last Name" id="" className="p-1 border-2 " />
                         </label>
                     </div>
                     {/* Email */}
                     <label className="form-control w-full">
                         <span className="label-text-alt text-lg">Email</span>
-                        <input type="email" placeholder="Email" id="" className="p-1 border-2" />
+                        <input type="email" name="email" placeholder="Email" id="" className="p-1 border-2" />
                        
                     </label>
                     {/* Address */}
                     <label className="form-control w-full">
                         <span className="label-text-alt text-lg">Address</span>
-                        <input type="text" placeholder="Address" id="" className="p-1 border-2" />
+                        <input type="text" name="address" placeholder="Address" id="" className="p-1 border-2" />
                     </label>
                     {/* Contact Number */}
                     <label className="form-control w-full">
                         <span className="label-text-alt text-lg">Contact Number</span>
-                        <input type="number" placeholder="Contact Number" id="" className="p-1 border-2" />
+                        <input type="number" name="contact" placeholder="Contact Number" id="" className="p-1 border-2" />
                     </label>
 
                     <div className="flex md:flex-row flex-col gap-5">
                         {/* City  */}
                         <label className="form-control w-full max-w-xs">
                             <span className="label-text-alt text-lg">City</span>
-                            <input type="text" placeholder="City" id="" className="p-1 border-2 " />
+                            <input type="text" name="city" placeholder="City" id="" className="p-1 border-2 " />
                         </label>
                         {/* State */}
                         <label className="form-control w-full max-w-xs">
                             <span className="label-text-alt text-lg">State</span>
-                            <input type="text" placeholder="State" id="" className="p-1 border-2 " />
+                            <input type="text" name="state" placeholder="State" id="" className="p-1 border-2 " />
                         </label>
                     </div>
                     {/* Password */}
                     <label className="form-control w-full">
                         <span className="label-text-alt text-lg">Password</span>
-                        <input type="password" placeholder="Password" id="" className="p-1 border-2" />
+                        <input type="password" name="password" placeholder="Password" id="" className="p-1 border-2" />
                     </label>
 
                 </form>
