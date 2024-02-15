57e5294d2006da451c4af084fff45d05453d46fc
MasudRana14 <iammasud34@gmail.com>
2024-02-16 00:00:57 +0600

    [Update](Design) Update Edit Profile Some Design

--- src/Components/EditProfile/EditProfile.js
+++ src/Components/EditProfile/EditProfile.js
@@ -1,6 +1,6 @@
+"use client"
+
 import Image from "next/image";
-
-
 const EditProfile = () => {
 
     const handleEdit = e=>{
@@ -14,82 +14,82 @@ const EditProfile = () => {
         const city = form.city.value;
         const state = form.state.value;
         const password = form.password.value;
-        e.target.form.value=" "
-
-        console.log(name1,name2,email,address,contact,city,state,password);
+        const result = {name1,name2,email,address,contact,city,state,password}
+        console.log(result);
     }
 
 
     return (
-        <div className="bg-red-200 my-10">
-        <div className="p-4">
+        <div className="bg-red-200 max-w-[1366px] shadow-xl mx-auto ">
+        <div className="p-3">
 
-            <div className="flex justify-around mx-20 md:mx-24 gap-5 my-4">
-                <h1 className="md:text-xl font-semibold">Setting Profile</h1>
-                <div className="avatar">
+            <div className="flex items-center  w-4/5  mx-auto gap-5 my-4">
+                <div className="avatar ">
                     <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                         <Image width={200} height={200} src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="" />
                     </div>
                 </div>
+                <h1 className="md:text-xl font-semibold">Edit Profile</h1>
             </div>
 
             {/* Input Form  */}
-            <div className="w-1/2 mx-auto my-10">
+            <div className="w-4/5 mx-auto my-10">
                 <form onSubmit={handleEdit} className="space-y-4">
 
                     <div className="flex md:flex-row flex-col gap-5">
                         {/* First Name  */}
-                        <label className="form-control w-full max-w-xs">
-                            <span className="label-text-alt text-lg">First Name</span>
+                        <label className="form-control w-full">
+                            <span className="label-text-alt font-medium text-lg">First Name</span>
                             <input type="text" name="name1" placeholder="First Name" id="" className="p-1 border-2 " />
                         </label>
                         {/* Last Name  */}
-                        <label className="form-control w-full max-w-xs">
-                            <span className="label-text-alt text-lg">Last Name</span>
+                        <label className="form-control w-full">
+                            <span className="label-text-alt font-medium text-lg">Last Name</span>
                             <input type="text" name="name2" placeholder="Last Name" id="" className="p-1 border-2 " />
                         </label>
                     </div>
                     {/* Email */}
                     <label className="form-control w-full">
-                        <span className="label-text-alt text-lg">Email</span>
+                        <span className="label-text-alt font-medium text-lg">Email</span>
                         <input type="email" name="email" placeholder="Email" id="" className="p-1 border-2" />
                        
                     </label>
                     {/* Address */}
                     <label className="form-control w-full">
-                        <span className="label-text-alt text-lg">Address</span>
+                        <span className="label-text-alt font-medium text-lg">Address</span>
                         <input type="text" name="address" placeholder="Address" id="" className="p-1 border-2" />
                     </label>
                     {/* Contact Number */}
                     <label className="form-control w-full">
-                        <span className="label-text-alt text-lg">Contact Number</span>
+                        <span className="label-text-alt font-medium text-lg">Contact Number</span>
                         <input type="number" name="contact" placeholder="Contact Number" id="" className="p-1 border-2" />
                     </label>
 
                     <div className="flex md:flex-row flex-col gap-5">
                         {/* City  */}
-                        <label className="form-control w-full max-w-xs">
-                            <span className="label-text-alt text-lg">City</span>
+                        <label className="form-control w-full">
+                            <span className="label-text-alt font-medium text-lg">City</span>
                             <input type="text" name="city" placeholder="City" id="" className="p-1 border-2 " />
                         </label>
                         {/* State */}
-                        <label className="form-control w-full max-w-xs">
-                            <span className="label-text-alt text-lg">State</span>
+                        <label className="form-control w-full">
+                            <span className="label-text-alt font-medium text-lg">State</span>
                             <input type="text" name="state" placeholder="State" id="" className="p-1 border-2 " />
                         </label>
                     </div>
                     {/* Password */}
                     <label className="form-control w-full">
-                        <span className="label-text-alt text-lg">Password</span>
+                        <span className="label-text-alt font-medium text-lg">Password</span>
                         <input type="password" name="password" placeholder="Password" id="" className="p-1 border-2" />
                     </label>
-
-                </form>
-
-                <div className="flex gap-7 my-7">
+                    
+                <div className="flex gap-7">
                     <button className="btn btn-sm btn-outline hover:bg-black border-[#F04336]">Cancel</button>
                     <button className="btn btn-sm text-white hover:bg-black bg-[#F04336]">Save</button>
                 </div>
+
+                </form>
+
 
             </div>
 
