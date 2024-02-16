482f430089bb95dfde077eab7e8f84966de497da
MasudRana14 <iammasud34@gmail.com>
2024-02-16 17:52:17 +0600

    [Add](Star Rating) Add Star Rating

--- src/Components/AddReview/AddReview.js
+++ src/Components/AddReview/AddReview.js
@@ -1,25 +1,44 @@
 
+import { useState } from 'react';
+import StarRatings from 'react-star-ratings';
+
 const AddReview = () => {
+
+    const [rating, setRating] = useState(5)
+
+   
+
 
     return (
         <div className="bg-red-100 rounded-md max-w-96 mx-auto">
 
-        <div className="p-4">
-            <div className="text-center">
-                <h1 className="text-xl font-semibold">WRITE A REVIEW</h1>
-                <p className="py-5">Feel free to share your opinion with everyone by give an review. Your review will display in homepage review section.</p>
-            </div>
-           
-            <form onSubmit={handleSubmit} className="flex justify-center flex-col">
-                <textarea className="textarea  textarea-error w-full" placeholder="Write your review min 50 character" name='review'></textarea>
-                <div className="flex justify-center my-4">
-                    <button className="btn btn-sm hover:bg-black  bg-[#F04336] text-white">Submit</button>
+            <div className="p-4">
+                <div className="text-center">
+                    <h1 className="text-xl font-semibold">WRITE A REVIEW</h1>
+                    <p className="py-5">Feel free to share your opinion with everyone by give an review. Your review will display in homepage review section.</p>
                 </div>
-            </form>
+                <div className='flex justify-center my-2' data-aos="zoom-in-right">
+                    <StarRatings
+                        rating={rating}
+                        starRatedColor="orange"
+                        starHoverColor="orange"
+                        changeRating={(r) => setRating(r)}
+                        numberOfStars={5}
+                        starDimension="25px"
+                        starSpacing="5px"
+                        name='rating'
+                    />
+                </div>
+                <form onSubmit={handleSubmit} className="flex justify-center flex-col">
+                    <textarea className="textarea  textarea-error w-full" placeholder="Write your review min 50 character" name='review'></textarea>
+                    <div className="flex justify-center my-4">
+                        <button className="btn btn-sm hover:bg-black  bg-[#F04336] text-white">Submit</button>
+                    </div>
+                </form>
+
+            </div>
 
         </div>
-
-    </div>
     );
 };
 
