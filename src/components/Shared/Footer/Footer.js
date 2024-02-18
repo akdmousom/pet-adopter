76b28d569bba79bdc52f3b0d96b56fee27275ea1
tahmid530 <tahmidahmed530@gmail.com>
2024-02-18 22:24:40 +0600

    [Update](Footer)Change footer design

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -17,16 +17,18 @@ const Footer = () => {
                     <p className="text-sm md:text-base"> Pet Adopter Limitied<br />Facilitating pet adoptions with trust and dedication since 1992.</p>
                 </aside>
                 <nav>
-                    <header className="footer-title text-[#F04336]">ABOUT US</header>
-                    <a style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Volunteer</a>
-                    <a style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">FAQ</a>
-                    <a style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Financials</a>
-                    <a style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Advertisement</a>
+                    <header className="footer-title text-[#F04336]">RESOURCES</header>
+                    <Link href='/about' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">About Us</Link>
+                    <Link href='/contact' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Contact Us</Link>
+                    <Link href='/shop' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Shop</Link>
+                    <Link href='/adoptHerePage' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Adopt Pet</Link>
                 </nav>
                 <nav>
                     <header className="footer-title text-[#F04336]">Company</header>
-                    <Link href='/about' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">About us</Link>
-                    <Link href='/contact' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Contact</Link>
+                    <Link href='/login' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Login</Link>
+                    <Link href='/register' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Register</Link>
+                    <Link href='/pet-listing' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Pet Listing</Link>
+                    <Link href='/' style={navStyle} className=" cursor-pointer hover:border-b-2 hover:border-[#F04336]">Home</Link>
 
                 </nav>
                 <nav>
