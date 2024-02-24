77185f8f100578494ac9d841c8194d819984391a
tahmid530 <tahmidahmed530@gmail.com>
2024-02-24 22:17:22 +0600

    [Image](Registration)Image send to Imgbb & got Imgbb link successfully

--- src/app/(allpage)/register/page.js
+++ src/app/(allpage)/register/page.js
@@ -1,4 +1,3 @@
-'use client'
 import Link from 'next/link';
 import React from 'react';
 import googleIcon from '../../../../public/images/google.svg'
@@ -7,11 +6,14 @@ import Image from 'next/image'
 import { POST } from '@/app/api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 
+const image_key = "a09d000658ca4fa5a3ce2dd2cac0530a";
+const image_api = `https://api.imgbb.com/1/upload?key=${image_key}`;
 
 const page = () => {
 
     const userData = async (formData) => {
-        // 'use server'
+
+        'use server'
         const rawFormData = {
             fullName: formData.get('userName'),
             userEmail: formData.get('userEmail'),
@@ -19,22 +21,19 @@ const page = () => {
             userPassword: formData.get('userPassword'),
         }
 
-        // When user hit the login button you can get user information in rawFormData object
-        await rawFormData
-        const res = await fetch(`https://pet-adopter-backend.vercel.app/api/v1/userregistration`, {
-            method: 'POST',
-            headers:{
-                "Content-Type": "application/json",
-                
-            },
-            body: JSON.stringify(rawFormData),
-        })
-        
-        if (res.ok) {
+        // const image_file = rawFormData.userAvater;
+        const formDataSend = new FormData();
+        formDataSend.append("key", image_key);
+        formDataSend.append("image", rawFormData.userAvater);
 
-            redirect('/')
-            
-        }
+        const response = await fetch(image_api, {
+            method: 'POST',
+            body: formDataSend,
+        })
+
+        const data = await response.json();
+        console.log(data);
+       
     }
 
 
@@ -65,34 +64,34 @@ const page = () => {
 
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Image URL</span>
+                                <span className="label-text">Image</span>
                             </label>
-                            <input type="text" name='userAvater' placeholder="Image URL" className="input input-bordered" required />
+                            <input type="file" name='userAvater' placeholder="Image URL" className="input input-bordered pt-2" required />
                         </div>
 
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name='userPassword' placeholder="password" className="input input-bordered" required />
+                            <input type="password" name='userPassword' placeholder="Password" className="input input-bordered" required />
 
                         </div>
 
                         <div className='mt-4'>
-                            <p className="text-sm text-center ">Already have an account <span className='text-[#f04336] font-bold'><Link href={'/login'}>Login</Link></span></p>
+                            <p className="text-sm text-center ">Already have an account? <span className='text-[#f04336] font-bold'><Link href={'/login'}>Login</Link></span></p>
                         </div>
 
                         <div className="form-control mt-6">
                             <button type='submit' className="btn bg-[#f04336] text-white hover:bg-black">Register</button>
                         </div>
-                        <hr/>
+                        <hr />
                         <p className="text-center">OR</p>
-                        <hr/>
+                        <hr />
                         <div className="form-control ">
-                            <button  type="submit" className="btn hover:bg-black hover:text-white gap-5"><Image src={googleIcon} alt='google icon' width={30} /> Login With Google</button>
+                            <button type="submit" className="btn hover:bg-black hover:text-white gap-5"><Image src={googleIcon} alt='google icon' width={30} /> Login With Google</button>
                         </div>
                         <div className="form-control ">
-                            <button  type="submit" className="btn hover:bg-black hover:text-white gap-5"><Image src={facebookIcon} alt='facebook icon' width={30}  /> Login With Facebook</button>
+                            <button type="submit" className="btn hover:bg-black hover:text-white gap-5"><Image src={facebookIcon} alt='facebook icon' width={30} /> Login With Facebook</button>
                         </div>
                     </form>
                 </div>
