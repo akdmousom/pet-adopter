b810d1a34b06ad8d7b420bbd86dc66fc3d26afd5
MasudRana14 <iammasud34@gmail.com>
2024-03-04 19:02:38 +0600

    [Remove](Code) Remove Some Old Code And Update New Design

--- src/components/contact-page/emailSection/page.js
+++ src/components/contact-page/emailSection/page.js
@@ -19,10 +19,9 @@ const EmailSection = () => {
     const target = e.target
     let name = target.user_name.value
     let email = target.user_email.value
-    // let subject = target.subject.value
     let message = target.message.value
     const rules = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
-    // console.log(name, email, subject, message)
+    
     if (name === '' || email === ''  || message === '') {
       Swal.fire({
         position: "center",
@@ -37,7 +36,6 @@ const EmailSection = () => {
         .then((result) => {
           target.user_name.value = ''
           target.user_email.value = ''
-          // target.subject.value = ''
           target.message.value = ''
           console.log(result.text);
           Swal.fire({
@@ -55,48 +53,29 @@ const EmailSection = () => {
 
 
   return (
-    <div className="mx-5 lg:mx-24">
+    <div className="mx-5 lg:mx-24 dark:text-white">
 
       <Script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></Script>
-
-      {/* <div className="bg-[#FFE3E1] dark:bg-[#AD8155] text-3xl lg:text-4xl text-center dark:text-white font-bold py-6 px-4 my-10">Contact us with email</div>
-      <div className="flex flex-col-reverse lg:flex-row py-10 px-10 lg:px-20">
-        <form ref={form} onSubmit={sendEmail} className="grid grid-cols-1 space-y-3 flex-1 py-10">
-          <input type="text" name='user_name' placeholder="Your Name" className="input input-bordered input-secondary w-full max-w-xs border-black" />
-          <input type="email" name='user_email' placeholder="Your E-mail" className="input input-bordered input-secondary w-full max-w-xs border-black" />
-          <input type="text" name='subject' placeholder="Subject" className="input input-bordered input-secondary w-full max-w-xs border-black" />
-          <textarea type='text' name='message' className="textarea textarea-secondary w-full max-w-xs border-black" placeholder="Your Massage"></textarea>
-          <button className="btn btn-active bg-[#F04336] dark:bg-gradient-to-r from-[#5edae8] to-[#12fbff] hover:bg-black font-semibold text-white dark:text-black w-full max-w-xs">Send email</button>
-
-        </form>
-        <div className="flex-1">
-          <Image src={img} className="p-5" alt='image of contact page' />
-        </div>
-      </div> */}
-
-
-
-      {/* New Design */}
 
       <div className="my-8  md:flex justify-between">
 
         <div className="">
           <div className="my-6 space-y-3">
             <h3 className="text-2xl font-medium">Contact Us</h3>
-            <h2 className="text-3xl font-bold">Lets Talk Question.</h2>
-            <p className="pt-4">The domestic dog is a doiated dendant of the wolf. The dog derived <br /> from an ancient, extinct wolf, and the modern grey.</p>
+            <h2 className="text-4xl font-bold">Lets Talk Question.</h2>
+            <p className="font-medium pt-4">The domestic dog is a doiated dendant of the wolf. The dog derived <br /> from an ancient, extinct wolf, and the modern grey.</p>
           </div>
           {/* Right Side  */}
           <div className="">
             <form ref={form} onSubmit={sendEmail} className="space-y-6">
               <label className="form-control">
                 <span className="label-text-alt font-medium text-base">Your Name</span>
-                <input type="text" name='user_name' required placeholder="Your Name" className="p-1 border-2 " />
+                <input type="text" name='user_name' required placeholder="Your Name" className="p-1 border-2 rounded-md" />
               </label>
 
               <label className="form-control">
                 <span className="label-text-alt font-medium text-base">Your Email</span>
-                <input type="email" name='user_email' required placeholder="Your Email" className="p-1 border-2 " />
+                <input type="email" name='user_email' required placeholder="Your Email" className="p-1 border-2 rounded-md" />
               </label>
 
               <label className="form-control">
@@ -104,7 +83,7 @@ const EmailSection = () => {
                 <textarea className="textarea textarea-error" type='text' name='message' placeholder="Opinion....."></textarea>
               </label>
 
-              <button className="btn bg-[#F04336] text-white font-medium hover:bg-black transition duration-500">Send Now</button>
+              <button className="btn bg-[#F04336] text-white font-medium hover:bg-black">Send Now</button>
 
             </form>
           </div>
