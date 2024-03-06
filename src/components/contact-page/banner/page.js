c6be177294e1b7ba5dc97f63e64449a3309d4109
MasudRana14 <iammasud34@gmail.com>
2024-03-06 23:28:00 +0600

    [Add](Banner) Add Contatc PAge Banner

--- src/components/contact-page/banner/page.js
+++ src/components/contact-page/banner/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
-import img from '../../../../public/images/contact-banner.png'
+import img from '../../../../public/images/contactcover.jpg'
 
 const ContactBanner = () => {
     return (
         <div>
-            <Image src={img} layout="responsive" alt="Contact page" />
+           <Image src={img} width={600} height={600} className="w-full h-[550px]" alt=""/>
+          
         </div>
     );
 };
