ec8efb20301715837dcfb7af7c2818a2e113e2b8
MasudRana14 <iammasud34@gmail.com>
2024-03-07 15:12:09 +0600

    [Change](Banner) Change Banner Image

--- src/components/contact-page/banner/page.js
+++ src/components/contact-page/banner/page.js
@@ -5,7 +5,7 @@ import img from '../../../../public/images/contactcover.jpg'
 const ContactBanner = () => {
     return (
         <div>
-           <Image src={img} width={600} height={600} className="w-full h-[550px]" alt=""/>
+           <Image src={img} width={600} height={600} className="w-full md:h-[550px]" alt=""/>
           
         </div>
     );
